c053d7c1f614bbb2fe98801a2bef8f1cd59d37fe
Raiosilva <raimundopi025@gmail.com>
2019-09-21 13:24:30 -0300

    ...

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,24 +37,43 @@ exports.createPages = ({ graphql, actions }) => {
           frontmatter {
             background
             category
-            date(locale: "pt-br", formatString:"DD [de] MMMM [de] YYYY")
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
             description
             title
           }
           timeToRead
         }
+        next {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+        previous {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
       }
     }
   }
+
   `).then(result => {
     const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(({ node }) => {
+    posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve("./src/templates/blog-post.js"),
         context: {
-          slug:node.fields.slug
+          slug:node.fields.slug,
+          previousPost: next,
+          nextPost: previous
         },
       })
     })
